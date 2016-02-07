9e8a1a51e4e58deadb02b50c26ce7644797d2a5f
Brian Murphy-Dye <brian@murphydye.com>
2016-02-07 10:34:08 -0700

    Add re-com library.

--- project.clj
+++ project.clj
@@ -54,6 +54,7 @@
                  [funcool/cats "1.2.1"]
                  [org.clojure/algo.monads "0.1.5"]
                  [clj-time "0.11.0"]
+                 [re-com "0.8.0"]
                  ]
 
   :min-lein-version "2.0.0"
