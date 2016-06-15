745c212669035a76884007a74c60e9a2ed28609c
Brian Murphy-Dye <brian@murphydye.com>
2016-06-15 14:54:35 -0600

    Add fake xml.

---
+++ src/timmus/routes/fake_axiall.clj
@@ -0,0 +1,48 @@
+(ns timmus.routes.fake-axiall
+  (:require ;; [clojure.test :as t]
+            ))
+
+
+(def fake-axiall
+  "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<!DOCTYPE cXML SYSTEM \"http://xml.cxml.org/schemas/cXML/1.2.014/cXML.dtd\">
+<cXML payloadID=\"1211221788.71299@ip-10-251-122-83\" timestamp=\"Mon May 19 18:29:48 +0000 2008\" xml:lang=\"en-US\">
+  <Header>
+    <From>
+      <Credential domain=\"DUNS\">
+        <Identity>coupa-t</Identity>
+      </Credential>
+    </From>
+    <To>
+      <Credential domain=\"DUNS\">
+        <Identity>coupa-t</Identity>
+      </Credential>
+    </To>
+    <Sender>
+      <Credential domain=\"DUNS\">
+        <Identity>axiall</Identity>
+        <SharedSecret>wi7oie.c</SharedSecret>
+      </Credential>
+      <UserAgent>myagent</UserAgent>
+    </Sender>
+  </Header>
+  <Request>
+    <PunchOutSetupRequest operation=\"create\">
+      <BuyerCookie>c64af92dc27e68172e030d3d</BuyerCookie>
+        <Extrinsic name=\"FirstName\">myfirstname</Extrinsic>
+      <Extrinsic name=\"LastName\">mylastname</Extrinsic>
+      <Extrinsic name=\"UniqueName\">myuniquename</Extrinsic>
+      <Extrinsic name=\"UserEmail\">myemail</Extrinsic>
+      <Extrinsic name=\"User\">myuser</Extrinsic>
+      <Extrinsic name=\"BusinessUnit\">mybusinessunit</Extrinsic>
+      <BrowserFormPost>
+        <URL>https://localhost:3449/api/punchout/accept-order-message/4</URL>
+      </BrowserFormPost>
+      <Contact role=\"myrole\">
+        <Name xml:lang=\"en-US\">jim</Name>
+        <Email>myemail</Email>
+      </Contact>
+    </PunchOutSetupRequest>
+  </Request>
+</cXML>"
+  )
