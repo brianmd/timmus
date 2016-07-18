9f9c7b1683ef6cb177173e3188fbfcb85373891d
Brian Murphy-Dye <brian@murphydye.com>
2016-07-17 18:26:38 -0600

    Add ability to develop using docker.

---
+++ docker/fig.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# note: must be run from the docker directory
+mkdir -p m2
+mkdir -p lein
+docker run -ti --rm -p 127.0.0.1:7002:7002 -p 127.0.0.1:3449:3449 -v $(pwd -P)/..:/root/docker -v $(pwd -P)/m2:/root/.m2 -v $(pwd -P)/lein:/root/.lein -v $(pwd -P)/../../utils:/root/utils timmus /bin/bash
---
+++ docker/prep.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# note: must be run from the docker directory
+
+# need setup from inside docker thread
+# cd docker
+ln -s /root/utils /root/docker/checkouts/utils
