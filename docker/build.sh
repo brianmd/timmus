7dc14bb92bd5d5a295fdecf42d154bf23e974597
Brian Murphy-Dye <brian@murphydye.com>
2016-08-03 09:26:08 -0600

    Generalize setting container's UID/GID to same as host's.

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-docker build -t timmus .
+# Get host's UID so can set container's UID/GID to the same.
+WHOAMI=`whoami`
+export USERID=`grep $WHOAMI /etc/passwd | cut -d: -f 3`
+docker build --build-arg USERID=$USERID -t timmus .
